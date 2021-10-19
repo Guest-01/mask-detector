bcd9f4b83ed3f373e8330385979509bb2f6b4957
atHome <cloudonly21@gmail.com>
2021-10-20 06:14:23 +0900

    add Dockerfile

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 wsgi_app = "mask_detector:create_app()"
-bind = "127.0.0.1:8001" # only on localhost. to expose, use 0.0.0.0
+bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = "access.log"
 errolog = "error.log"
