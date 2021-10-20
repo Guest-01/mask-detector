3dc09107176e560c90273dd15c6a40c08d769c29
onMac <cloudonly21@gmail.com>
2021-10-20 20:47:00 +0900

    change loglevel to debug

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,5 +4,5 @@ wsgi_app = "mask_detector:create_app()"
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = "access.log"
-errolog = "error.log"
+loglevel="debug"
 timeout = 120
